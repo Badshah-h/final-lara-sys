97c522f83e32b21bcd8f0aaefe333a36b1ca4fcb
badshah-h <haroon4.dev@gmail.com>
2025-10-26 12:29:42 +0000

    update

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 
-import { TempoDevtools } from "tempo-devtools";
-TempoDevtools.init();
+/* import { TempoDevtools } from 'tempo-devtools'; [deprecated] */
+/* TempoDevtools.init() [deprecated] */;
 
 const basename = import.meta.env.BASE_URL;
 
